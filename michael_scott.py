a2a3e50e05a89c4362141ee073d2f96f0433d54b
zssherman <shermanx1991@gmail.com>
2019-05-30 14:38:22 -0500

    ENH: Added numpy ad reference rick and morty.

--- michael_scott.py
+++ michael_scott.py
@@ -1,2 +1,4 @@
 print('I am not supersitious... Just a little stitious')
 print("well I've been going through a little rough patch... the whole year actually.")
+import numpy
+print('wubba lubba dub dub')
